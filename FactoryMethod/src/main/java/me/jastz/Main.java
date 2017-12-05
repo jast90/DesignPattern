32d01f9cd0a997ec68ba1611ac0f4e8654758053
Jast <zhangzhiwen90@gmail.com>
2017-12-05 23:51:44 +0800

    Factory method pattern

---
+++ FactoryMethod/src/main/java/me/jastz/Main.java
@@ -0,0 +1,11 @@
+package me.jastz;
+
+/**
+ * 工厂方法模式又称工厂模式，也叫虚拟构造器模式或多态工厂模式
+ * ，通过工厂子类来确定究竟应该实例化哪个具体产品类
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
